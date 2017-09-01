bace56c0728017c4befffa8eb99b87406d8bf702
Hosein Rezaei <mr.hrezaei@gmail.com>
2017-09-01 21:43:43 +0430

    The first block of code is refactorred in palce

--- Statistics.php
+++ Statistics.php
@@ -3,43 +3,42 @@
 
 class Statistics
 {
- /**
- * @param string $candidate
- * @param int $count
- *
- * @internal string $number
- * @internal string $verb
- * @internal string $pluralModifier
- */
-    public function printGuessStatistics($candidate, $count)
+    /**
+     * @param string $candidate
+     * @param int $count
+     *
+     * @internal string $number
+     * @internal string $verb
+     * @internal string $pluralModifier
+     */
+    private $number = 'no';
+    private $verb = 'are';
+    private $pluralModifier = 's';
+    
+    public function printGuessStatistics($candidate, $count = 0)
     {
-        if ($count == 0) {
-            $number = "no";
-            $verb = "are";
-            $pluralModifier = "s";
-        } elseif ($count == 1) {
-            $number = "1";
-            $verb = "is";
-            $pluralModifier = "";
-        } else {
-            $number = $count;
-            $verb = "are";
-            $pluralModifier = "s";
+        if ($count == 1) {
+            $this->number = "1";
+            $this->verb = "is";
+            $this->pluralModifier = "";
+        } elseif($count > 1) {
+            $this->number = $count;
         }
-        printf("There %s %s %s%s<br/>", 
-            $verb,
-            $number,
+        printf("There %s %s %s%s<br/>",
+            $this->verb,
+            $this->number,
             $candidate,
-            $pluralModifier
-        );
+            $this->pluralModifier
+            );
     }
 }
 /**
-* What's the output?
-*/
+ * What's the output?
+ */
 $statistics = new Statistics();
 $statistics->printGuessStatistics("bird", 2);
 $statistics = new Statistics();
-$statistics->printGuessStatistics("bird", 0);
+$statistics->printGuessStatistics("bird");
 $statistics = new Statistics();
 $statistics->printGuessStatistics("bird", 1);
+
